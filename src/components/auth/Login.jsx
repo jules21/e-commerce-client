9e7938e750d39814d9c2849258d2cb0e7a7e76ca
jules21 <julesfabien96@gmail.com>
2022-05-01 06:40:44 +0200

    login page

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import axios from 'axios'
 
 function Login() {
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+      .post('http://127.0.0.1:8000/api/login', {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response)
+      })
+  }
   return (
-    <div>Login</div>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
