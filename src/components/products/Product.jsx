6547fc9862b58c82cead80c9d90b796911017ca0
jules21 <julesfabien96@gmail.com>
2022-05-01 06:50:34 +0200

    get products from api

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ProductItem from './ProductItem'
+import axios from 'axios';
 
 
-function Product({products}) {
+function Product() {
+
+  const [products,setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/products')
+    .then(response => {
+        setProducts(response.data)
+    })
+    .catch(error => console.error(error));
+}, []);
+
   return (
     <div>
         {products.map(product => (
@@ -12,4 +24,4 @@ function Product({products}) {
   )
 }
 
-export default Product
+export default Product;
--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-function ProductItem() {
+function ProductItem({product}) {
   return (
-    <div>ProductItem</div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">{product.name}</h5>
+            <p class="card-text">{product.description}</p>
+            <a href="#" class="card-link">Buy Now</a>
+            <a href="#" class="card-link">{product.price}</a>
+          </div>
+        </div>
   )
 }
 
